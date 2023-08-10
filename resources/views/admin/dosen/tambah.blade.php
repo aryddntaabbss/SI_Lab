e2095bb19c028a0226815b603de8f12b1650a664
aryaddinata <aryaddinataabbas017@gmail.com>
2023-08-10 16:37:10 +0900

    bombastic side eye

--- resources/views/admin/dosen/tambah.blade.php
+++ resources/views/admin/dosen/tambah.blade.php
@@ -18,30 +18,33 @@
         <div class="container-fluid">
             <div class="col-12">
 
-                <form method="POST" action="{{ route('dosen.create') }}" >
+                <form method="POST" action="{{ route('dosen.create') }}">
                     @csrf
                     <div class="col-10 mx-3 mt-3 mb-1">
                         <div class="mb-3">
                             <label for="nip" class="form-label">Nip :</label>
-                            <input type="text" class="form-control" id="nip" placeholder="NIP Dosen" name="id">
+                            <input type="text" class="form-control" id="nip" placeholder="NIP Dosen"
+                                name="id">
                         </div>
 
                         <div class="mb-3">
                             <label for="nama" class="form-label">Nama :</label>
-                            <input type="text" class="form-control" id="nama" placeholder="Username" name="name" value="{{ old('name') }}">
+                            <input type="text" class="form-control" id="nama" placeholder="Username" name="name"
+                                value="{{ old('name') }}">
                             <x-input-error :messages="$errors->get('name')" class="mt-2 text-danger" />
                         </div>
                         <div class="mb-3">
                             <label for="prodi" class="form-label">Prodi :</label>
                             <select class="form-control" id="prodi" name="prodi">
-                                @foreach($prodis as $prodi)
+                                @foreach ($prodis as $prodi)
                                     <option value="{{ $prodi->id }}">{{ $prodi->nama_prodi }}</option>
                                 @endforeach
                             </select>
                         </div>
                         <div class="mb-3">
                             <label for="email" class="form-label">Email :</label>
-                            <input type="email" class="form-control" id="email" placeholder="tes@gmail.com" name="email">
+                            <input type="email" class="form-control" id="email" placeholder="tes@gmail.com"
+                                name="email">
                             <x-input-error :messages="$errors->get('email')" class="mt-2 text-danger" />
                         </div>
                         <div class="row mb-3 justify-content-between">
@@ -54,14 +57,26 @@
                                 </div>
                                 <div class="mb-3">
                                     <label for="password" class="form-label">Password :</label>
-                                    <input type="password" class="form-control" id="password" name="password">
-                                    <x-input-error :messages="$errors->get('password')" class="mt-2 text-danger" /> 
+                                    <div class="input-group">
+                                        <input type="password" class="form-control" id="password" name="password">
+                                        <button class="btn btn-outline-hidden" type="button" id="togglePassword">
+                                            <i class="fa fa-eye-slash" aria-hidden="true"></i>
+                                        </button>
+                                    </div>
+                                    <x-input-error :messages="$errors->get('password')" class="mt-2 text-danger" />
                                 </div>
                                 <div class="mb-3">
                                     <label for="password_confirmation" class="form-label">Konfirmasi Password :</label>
-                                    <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" required autocomplete="new-password">
+                                    <div class="input-group">
+                                        <input type="password" class="form-control" id="password_confirmation"
+                                            name="password_confirmation" required autocomplete="new-password">
+                                        <button class="btn btn-outline-hidden" type="button" id="toggleConfirmPassword">
+                                            <i class="fa fa-eye-slash" aria-hidden="true"></i>
+                                        </button>
+                                    </div>
                                     <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2 text-danger" />
                                 </div>
+
                             </div>
                             <div class="col-4">
                                 <p class="mb-2">Password requirements</p>
@@ -72,10 +87,10 @@
                                     <li>At least one special character</li>
                                     <li>At least one number</li>
                                     <li>Can’t be the same as a previous password </li>
-                                </ul>    
+                                </ul>
                             </div>
                         </div>
-                        
+
                     </div>
 
                     <div class="d-grid gap-2 mx-4 mt-3">
@@ -91,6 +106,30 @@
         <!-- /.content -->
     </div>
     <!-- /.content-wrapper -->
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            const togglePasswordButton = document.getElementById('togglePassword');
+            const toggleConfirmPasswordButton = document.getElementById('toggleConfirmPassword');
+            const passwordField = document.getElementById('password');
+            const confirmPasswordField = document.getElementById('password_confirmation');
+
+            togglePasswordButton.addEventListener('click', function() {
+                if (passwordField.type === 'password') {
+                    passwordField.type = 'text';
+                } else {
+                    passwordField.type = 'password';
+                }
+            });
+
+            toggleConfirmPasswordButton.addEventListener('click', function() {
+                if (confirmPasswordField.type === 'password') {
+                    confirmPasswordField.type = 'text';
+                } else {
+                    confirmPasswordField.type = 'password';
+                }
+            });
+        });
+    </script>
 
     @include('admin.layouts.footer')
 @endsection
